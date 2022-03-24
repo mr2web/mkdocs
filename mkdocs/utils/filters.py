02b71986baede25340c5b83850db3359f75e99d9
Tobias Eriksson <mr2web@hotmail.com>
2022-03-25 00:53:18 +0100

    Fixed Jinja2 depricated naming of 'contextfiler'. Replace it with 'pass_contex'. See https://jinja.palletsprojects.com/en/3.0.x/changes/

--- mkdocs/utils/filters.py
+++ mkdocs/utils/filters.py
@@ -3,7 +3,7 @@ import jinja2
 from mkdocs.utils import normalize_url
 
 
-@jinja2.contextfilter
+@jinja2.pass_context
 def url_filter(context, value):
     """ A Template filter to normalize URLs. """
     return normalize_url(value, page=context['page'], base=context['base_url'])
